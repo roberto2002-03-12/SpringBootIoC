549e7f431cda15d1b47bae932d3d15e24ef4158f
roberto2002-03-12 <robertog-18@hotmail.com>
2024-12-29 21:24:20 -0500

    feat: ciclo de vida de una clase

--- src/main/java/net/rob/projects/SpringBootIoC/service/ProductService.java
+++ src/main/java/net/rob/projects/SpringBootIoC/service/ProductService.java
@@ -3,6 +3,8 @@ package net.rob.projects.SpringBootIoC.service;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import net.rob.projects.SpringBootIoC.repository.IProductRepository;
 
 @Service
@@ -12,6 +14,16 @@ public class ProductService {
   public ProductService(@Qualifier("productRepositoryA") IProductRepository productRepository) {
     this.productRepository = productRepository;
     System.out.println("ProductService: constructor");
+  }
+
+  @PostConstruct
+  public void postConstruct() {
+    System.out.println("ProductService: postConstruct");
+  }
+
+  @PreDestroy // al momento de cerrar la aplicacion se ejecuta este metodo
+  public void preDestroy() {
+    System.out.println("ProductService: preDestroy");
   }
 
   public void save(String name) {
